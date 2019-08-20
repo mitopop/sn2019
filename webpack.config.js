462c44a6e088e600466b1c2543cfac7c13ae5125
ulrichsinn <uli@addtothenoise.com>
2019-08-19 21:32:36 -0700

    rc files

---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const glob = require('glob');
+
+const parts = require('./webpack.parts');
+
+const PATHS = {
+  app: path.join(__dirname, 'src'),
+};
+
+const commonConfig = merge([
+  {
+    output: {
+      // Needed for code splitting to work in nested paths
+      publicPath: '/',
+    },
+    resolveLoader: {
+      alias: {
+        'demo-loader': path.resolve(
+          __dirname,
+          'loaders/demo-loader.js'
+        ),
+      },
+    },
+  },
+  parts.loadFonts({
+    options: {
+      // name: '[name].[hash:8].[ext]',
+    },
+  }),
+  parts.loadJavaScript({ include: PATHS.app }),
+  parts.setFreeVariable('HELLO', 'hello from config'),
+]);
+
+const productionConfig = merge([
+  {
+    performance: {
+      hints: 'warning', // "error" or false are valid too
+      maxEntrypointSize: 150000, // in bytes, default 250k
+      maxAssetSize: 450000, // in bytes
+    },
+  },
+  {
+    output: {
+      chunkFilename: '[name].[chunkhash:4].js',
+      filename: '[name].[chunkhash:4].js',
+    },
+  },
+  parts.clean(),
+  parts.minifyJavaScript(),
+  parts.minifyCSS({
+    options: {
+      discardComments: {
+        removeAll: true,
+      },
+      // Run cssnano in safe mode to avoid
+      // potentially unsafe transformations.
+      safe: true,
+    },
+  }),
+  parts.extractCSS({
+    use: ['css-loader', parts.autoprefix()],
+  }),
+  parts.purifyCSS({
+    paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true }),
+  }),
+  parts.loadImages({
+    options: {
+      limit: 15000,
+      name: '[name].[hash:4].[ext]',
+    },
+  }),
+  parts.generateSourceMaps({ type: 'source-map' }),
+  {
+    optimization: {
+      splitChunks: {
+        chunks: 'initial',
+      },
+      runtimeChunk: {
+        name: 'manifest',
+      },
+    },
+  },
+  // parts.attachRevision(),
+]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+  parts.loadCSS(),
+  parts.loadImages(),
+]);
+
+module.exports = mode => {
+  const pages = [
+    parts.page({
+      title: 'Seagan Ngai',
+      template: require.resolve('./template/default_index.ejs'),
+      entry: {
+        app: mode === 'production' ? PATHS.app :
+          ['react-hot-loader/patch', PATHS.app],
+      },
+      chunks: ['manifest', 'app', 'vendors~app'],
+    }),
+  ];
+  const config =
+    mode === 'production' ? productionConfig : developmentConfig;
+
+  return merge([commonConfig, config, { mode }].concat(pages));
+};
---
+++ webpack.parts.js
@@ -0,0 +1,182 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require('webpack');
+//const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const cssnano = require('cssnano');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+exports.page = ({
+  path = '',
+  template = require.resolve(
+    'html-webpack-plugin/default_index.ejs'
+  ),
+  title,
+  entry,
+  chunks,
+} = {}) => ({
+  entry,
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks,
+      filename: `${path && path + '/'}index.html`,
+      meta: {
+        viewport: 'width=device-width, initial-scale=1',
+      },
+      template,
+      title,
+    }),
+  ],
+});
+
+exports.minifyCSS = ({ options }) => ({
+  plugins: [
+    new OptimizeCSSAssetsPlugin({
+      cssProcessor: cssnano,
+      cssProcessorOptions: options,
+      canPrint: false,
+    }),
+  ],
+});
+
+exports.minifyJavaScript = () => ({
+  optimization: {
+    minimizer: [new UglifyWebpackPlugin({ sourceMap: true })],
+  },
+});
+
+// exports.attachRevision = () => ({
+//   plugins: [
+//     new webpack.BannerPlugin({
+//       banner: new GitRevisionPlugin().version(),
+//     }),
+//   ],
+// });
+
+exports.clean = () => ({
+  plugins: [new CleanWebpackPlugin()],
+});
+
+exports.purifyCSS = ({ paths }) => ({
+  plugins: [new PurifyCSSPlugin({ paths })],
+});
+
+exports.extractCSS = ({ include, exclude, use = [] }) => {
+  // Output extracted CSS to a file
+  const plugin = new MiniCssExtractPlugin({
+    filename: '[name].[contenthash:4].css',
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+
+          use: [MiniCssExtractPlugin.loader].concat(use),
+        },
+      ],
+    },
+    plugins: [plugin],
+  };
+};
+
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    stats: 'errors-only',
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    open: true,
+    overlay: true,
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
+
+exports.autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => [require('autoprefixer')()],
+  },
+});
+
+exports.loadImages = ({ include, exclude, options } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        include,
+        exclude,
+        use: {
+          loader: 'url-loader',
+          options,
+        },
+      },
+    ],
+  },
+});
+
+exports.loadJavaScript = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        exclude,
+        use: 'babel-loader',
+      },
+    ],
+  },
+});
+
+
+exports.loadFonts = function ({ include, exclude, options } = {}) {
+  return {
+    module: {
+      rules: [
+        {
+          // Capture eot, ttf, woff, and woff2
+          test: /\.(eot|ttf|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+          include,
+          exclude,
+
+          use: {
+            // loader: 'file-loader',
+            loader: 'url-loader?limit=20&name=assets/[name].[hash].[ext]',
+            options,
+          },
+        },
+      ],
+    },
+  };
+};
+
+
+exports.generateSourceMaps = ({ type }) => ({
+  devtool: type,
+});
+
+exports.setFreeVariable = (key, value) => {
+  const env = {};
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [new webpack.DefinePlugin(env)],
+  };
+};
